03b7dc8ed8c5671b44389b7a3ec81cd3a616ff06
Basil Okache <basilokache@gmail.com>
2023-03-25 10:53:09 +0100

    Added the images and icons

---
+++ wp-content/themes/asauk/front-page.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Template name: Front Page
+ * The template for displaying all single posts
+ * 
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ * 
+ * @package asauk
+ */
+
+    get_header();
+    $post_id = get_the_ID();
+?>
+
+<main>
+    
+</main>
--- wp-content/themes/asauk/header.php
+++ wp-content/themes/asauk/header.php
@@ -29,20 +29,7 @@
 		<div class="site-branding">
 			<?php
 			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$asauk_description = get_bloginfo( 'description', 'display' );
-			if ( $asauk_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $asauk_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
+			?>
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
