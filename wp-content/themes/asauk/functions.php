75b15e04b1aff408b23470379e3df31e166b0568
Basil Okache <basilokache@gmail.com>
2023-03-25 11:26:22 +0100

    working on the header

--- wp-content/themes/asauk/functions.php
+++ wp-content/themes/asauk/functions.php
@@ -49,7 +49,7 @@ function asauk_setup() {
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus(
 		array(
-			'menu-1' => esc_html__( 'Primary', 'asauk' ),
+			'primary-menu' => esc_html__( 'Primary', 'asauk' ),
 		)
 	);
 
--- wp-content/themes/asauk/header.php
+++ wp-content/themes/asauk/header.php
@@ -47,12 +47,16 @@
 			<nav id="site-navigation" class="main-navigation">
 				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'asauk' ); ?></button>
 				<?php
-				wp_nav_menu(
-					array(
-						'theme_location' => 'menu-1',
-						'menu_id'        => 'primary-menu',
-					)
-				);
+					wp_nav_menu(
+						array(
+							'theme_location' 	=> 'primary-menu',
+							'container'			=> 'div',
+							'container_class'	=> 'navbar-list',
+							'container_id'		=> 'navbar-list',
+							'menu_id'        	=> 'primary-menu',
+							'items_wrap' 		=> '<ul class="menu-list">%3$s</ul'
+						)
+					);
 				?>
 			</nav><!-- #site-navigation -->
 		</div>
