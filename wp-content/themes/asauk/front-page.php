5546615fe64d4ac1bc69d4f89f5c65f43877baa3
Basil Okache <basilokache@gmail.com>
2023-04-08 12:18:49 +0100

    styling the news section

--- wp-content/themes/asauk/front-page.php
+++ wp-content/themes/asauk/front-page.php
@@ -111,7 +111,7 @@
                                 <span class=""><h6>RAS News</h6></span>
                             </div>
                             <div>
-                                <span><h4>Key findings from Parliamentary meeting on UK VISA refusals for African Visitors to the UK</h4></span>
+                                <span><h4>Key findings from parliamentary meeting on UK VISA refusals for African Visitors to the UK</h4></span>
                                 <span></span>
                                 <span></span>
                             </div>
