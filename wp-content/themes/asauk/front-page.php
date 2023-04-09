aa8d9ba9ae4dc343e5194b14c34a9c010ffeb27a
Basil Okache <basilokache@gmail.com>
2023-04-09 20:19:09 +0100

    styling the news section

--- wp-content/themes/asauk/front-page.php
+++ wp-content/themes/asauk/front-page.php
@@ -111,7 +111,7 @@
                                 <span class="news-span-1"><h6>RAS News</h6></span>
                             </div>
                             <div class="col-container">
-                                <span><h4>Key findings from parliamentary meeting on UK VISA refusals for African Visitors to the UK</h4></span>
+                                <span class="news-title"><h4>Key findings from parliamentary meeting on UK VISA refusals for African Visitors to the UK</h4></span>
                                 <span><h6>Eget nulla facilisi etiam dignissim diam quis enim lobortis scelerisque. Eu feugiat pretium nibh ipsum consequat nisl vel pretium. Eget nulla facilisi etiam dignissim diam quis enim.</h6></span>
                                 <span></span>
                             </div>
@@ -142,7 +142,7 @@
                             </div>
                             <div class="col-container">
                                 <span class="news-title"><h4>Conflicts, Coups and Democracy: The African Union and the Second Arab Spring</h4></span>
-                                <span><h6>yeah, my boyfriend's pretty cool, but he's not as cool as me. Eget nulla facilisi etiam dignissim diam quis enim lobortis scelerisque. Eu feugiat pretium nibh ipsum consequat nisl vel pretium. Eget nulla facilisi etiam dignissim diam quis enim.</h6></span>
+                                <span><h6>Eget nulla facilisi etiam dignissim diam quis enim lobortis scelerisque. Eu feugiat pretium nibh ipsum consequat nisl vel pretium. Eget nulla facilisi etiam dignissim diam quis enim.</h6></span>
                                 <span></span>
                             </div>
                         </div>
