8481a5d149bacec44981206b4c5bfb81042329bc
Basil Okache <basilokache@gmail.com>
2023-04-09 21:18:21 +0100

    styling the news section

--- wp-content/themes/asauk/front-page.php
+++ wp-content/themes/asauk/front-page.php
@@ -113,7 +113,13 @@
                             <div class="col-container">
                                 <span class="news-title"><h4>Key findings from parliamentary meeting on UK VISA refusals for African Visitors to the UK</h4></span>
                                 <span class="news-content"><h6>Eget nulla facilisi etiam dignissim diam quis enim lobortis scelerisque. Eu feugiat pretium nibh ipsum consequat nisl vel pretium. Eget nulla facilisi etiam dignissim diam quis enim.</h6></span>
-                                <span></span>
+                                <span class="news-more">
+                                    <span class="read-more">
+                                        <h6>Read more</h6>
+                                        <h6><img src="<?php echo get_template_directory_uri()?> /assets/icons/arrow.png" alt="arrow"></h6>
+                                    </span>
+                                    <span class="news-date"><h6>30/05/19</h6></span>
+                                </span>
                             </div>
                         </div>
                         <div class="news-items" id="news-items">
