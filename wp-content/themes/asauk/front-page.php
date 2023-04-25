23c0c7c71fbd3f068a030fa316e87908623e2861
Basil Okache <basilokache@gmail.com>
2023-04-25 01:42:35 +0100

    few additions to the frontpage content

--- wp-content/themes/asauk/front-page.php
+++ wp-content/themes/asauk/front-page.php
@@ -154,7 +154,7 @@
                             </div>
                             <div class="col-container">
                                 <span class="news-title"><h4>Conflicts, Coups and Democracy: The African Union and the Second Arab Spring</h4></span>
-                                <span class="news-content"><h6>I was wrong, I was wrong, now I'll never see your face, anymore. Eu feugiat pretium nibh ipsum consequat nisl vel pretium. Eget nulla facilisi etiam dignissim diam quis enim.</h6></span>
+                                <span class="news-content"><h6>I was wrong, I was wrong, now I'll never see your face, anymore. Oh my love, I'll give anything for one more day with you. .</h6></span>
                                 <span class="news-more">
                                     <span class="read-more">
                                         <a href="#"><h6>Read more</h6>
